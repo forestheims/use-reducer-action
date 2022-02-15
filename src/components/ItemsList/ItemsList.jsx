c22496f1c2ed266970df615aebb373099ca23443
forestheims <forestheims@gmail.com>
2022-02-14 17:48:35 -0800

    reducer not complete, contains misplaced code at the moment

---
+++ src/components/ItemsList/ItemsList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ItemsList() {
+  return (
+    <div>ItemsList</div>
+  )
+}
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
+import ItemsList from '../components/ItemsList/ItemsList'
 
 export default function Home() {
+    const [items, dispatch] = useReducer(itemsReducer, [])
+    const itemsReducer = (state, action) => {
+        switch(action.type) {
+            case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    default:
+      throw new Error();
+        }
+    }
   return (
-    <div>Home</div>
+    <div>
+        <ItemsList items={items} />
+    </div>
   )
 }
