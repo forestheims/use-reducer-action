382c669684431a1d2a9a3b7bc9b94f5a5523e9dd
forestheims <forestheims@gmail.com>
2022-02-14 17:21:43 -0800

    created layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Home from './views/Home';
+import Layout from './views/Layout/Layout';
 
 function App() {
   return (
     <div className="App">
-
+      <Layout>
+        <Home />
+      </Layout>
     </div>
   );
 }
