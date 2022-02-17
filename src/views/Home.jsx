c49febc7a3a63a45ea891d4ae5b7d6f7e60119a9
forestheims <forestheims@gmail.com>
2022-02-17 08:54:41 -0800

    itemsReducer updated, still needs work and implementation.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,46 +1,50 @@
-import React, { useReducer } from 'react'
-import ItemControls from '../components/ItemControls/ItemControls';
-import ItemsList from '../components/ItemsList/ItemsList'
+import React, { useReducer } from "react";
+import ItemControls from "../components/ItemControls/ItemControls";
+import ItemsList from "../components/ItemsList/ItemsList";
 
 export default function Home() {
-  
-  const itemsReducer = (state, action) => {
-    switch(action.type) {
-      case 'add':
-        return {items: [...state.items, {id: items.length, content: }]};
-        case 'delete':
-          return {items: items.filter((item) => item.id !== id)};
-          case 'edit':
-            return {};
-          default:
-            throw new Error();
-          }
-        }
+  const itemsReducer = (state, { type, payload }) => {
+    switch (type) {
+      case "add":
+        return {
+          items: [...state.items, { id: items.length, content: payload }],
+        };
+      case "delete":
+        return { items: items.filter((item) => item.id !== payload.id) };
+      case "edit":
+        return { items: items.map((item) => item.id === payload.item.id) };
+      default:
+        throw new Error();
+    }
+  };
 
-  const [items, dispatch] = useReducer(itemsReducer, [])
+  const [items, dispatch] = useReducer(itemsReducer, []);
 
-  const addItem = () => {
+  const addItem = (entry) => {
     dispatch({
-      type: 'add'
-    })
-  }
+      type: "add",
+      payload: entry,
+    });
+  };
 
-  const deleteItem = () => {
+  const deleteItem = (id) => {
     dispatch({
-      type: 'delete'
-    })
-  }
+      type: "delete",
+      payload: id,
+    });
+  };
 
-  const editItem = () => {
+  const editItem = (item) => {
     dispatch({
-      type: 'edit'
-    })
-  }
-  
+      type: "edit",
+      payload: item,
+    });
+  };
+
   return (
     <>
       <ItemControls addItem={addItem} />
       <ItemsList items={items} deleteItem={deleteItem} editItem={editItem} />
     </>
-  )
+  );
 }
