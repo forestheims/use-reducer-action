494f7a68bd311d0feed720b8fe13fe1848bf172f
forestheims <forestheims@gmail.com>
2022-02-18 08:30:47 -0800

    add item now functions

--- src/components/ItemControls/ItemControls.jsx
+++ src/components/ItemControls/ItemControls.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function ItemControls({ addItem }) {
+  const [entry, setEntry] = useState("");
   return (
-    <div>ItemControls</div>
-  )
+    <div>
+      <label>New Item:</label>
+      <input
+        type="text"
+        value={entry}
+        onChange={(e) => setEntry(e.target.value)}
+      />
+      <button onClick={() => addItem(entry)}>Save Item</button>
+    </div>
+  );
 }
--- src/components/ItemsList/ItemsList.jsx
+++ src/components/ItemsList/ItemsList.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function ItemsList({ items, deleteItem, editItem }) {
+  console.log(items);
   return (
-    <div className="items-list">{items.map((item) => (
-      <div key={item.id}>
-        <span>{item.content}</span>
-        <button onClick={deleteItem}>delete</button>
-        <button onClick={editItem}>edit</button>
-      </div>
-    )
-    )}</div>
-  )
+    <div className="items-list">
+      {items.map((item) => (
+        <div key={item.id}>
+          <div>{item.text}</div>
+          <button onClick={deleteItem}>delete</button>
+          <button onClick={editItem}>edit</button>
+        </div>
+      ))}
+    </div>
+  );
 }
