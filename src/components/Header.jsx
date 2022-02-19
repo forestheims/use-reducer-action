af48dff70da779c8d08f3f935bd2a1d2c6008786
forestheims <forestheims@gmail.com>
2022-02-18 17:06:40 -0800

    number of items on the shopping list is displayed in the header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { useItems } from "../context/ItemsContext";
 
 export default function Header() {
-  return <header>Shopping List</header>;
+  const { items } = useItems();
+  return (
+    <header>
+      {items.length === 0 ? (
+        "Your shopping list is empty"
+      ) : (
+        <span>You have {items.length} items on your list</span>
+      )}
+    </header>
+  );
 }
